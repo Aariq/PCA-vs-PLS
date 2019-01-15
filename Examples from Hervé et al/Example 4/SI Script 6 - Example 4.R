65fe46228e9dee9d0de69fc053b6d1dc26f765c8
Aariq <scottericr@gmail.com>
2019-01-15 12:51:43 -0500

    minor edits to make it work in this directory

--- Examples from Hervé et al/Example 4/SI Script 6 - Example 4.R
+++ Examples from Hervé et al/Example 4/SI Script 6 - Example 4.R
@@ -3,14 +3,17 @@
 library(mixOmics)      # Functions needed: block.plsda, plotDiablo, circosPlot, cimDiablo,
                        #                   network
 library(RVAideMemoire) # Functions needed: DIABLO.cv, DIABLO.test, MVA.synt, MVA.plot
+library(here)
 
 
 
 ## Data loading
 
-tab.Perianths <- read.table("Example 4 - Perianths.txt",header=TRUE)
+tab.Perianths <- read.table(here("Examples from Hervé et al","Example 4", "Example 4 - Perianths.txt"), 
+                            header=TRUE)
 
-tab.Anthers <- read.table("Example 4 - Anthers.txt",header=TRUE)
+tab.Anthers <- read.table(here("Examples from Hervé et al", "Example 4", "Example 4 - Anthers.txt"),
+                               header=TRUE)
 
 Genotype <- tab.Anthers$Genotype
 
@@ -18,19 +21,20 @@ Genotype <- tab.Anthers$Genotype
 
 ## Pre-treatment
 
-Chemistry.Perianths <- scale(tab.Perianths[,2:41])
-Chemistry.Anthers <- scale(tab.Anthers[,2:44])
-Chemistry <- list(Perianths=Chemistry.Perianths,Anthers=Chemistry.Anthers)
+Chemistry.Perianths <- scale(tab.Perianths[ , 2:41])
+Chemistry.Anthers <- scale(tab.Anthers[ , 2:44])
+Chemistry <- list(Perianths = Chemistry.Perianths, Anthers = Chemistry.Anthers)
 
 
 
 ## Analysis
 
-DIABLO <- block.plsda(Chemistry,Genotype,ncomp=2)
+DIABLO <- block.plsda(Chemistry, Genotype, ncomp = 2)
 
 DIABLO.cv(DIABLO)
+
 # This may take several mninutes to run, remove progress=FALSE to see computation progress:
-DIABLO.test(DIABLO,progress=FALSE) # Remove progress=FALSE to see computation progress
+DIABLO.test(DIABLO, progress = TRUE) # Remove progress=FALSE to see computation progress
 MVA.synt(DIABLO)
 
 MVA.plot(DIABLO,fac=Genotype,space=1,drawextaxes=FALSE,main="Perianths")
