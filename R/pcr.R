2bb30b9e143de36b4f93b5d53627343c55a5bdfa
Aariq <scottericr@gmail.com>
2019-05-01 14:21:27 -0400

    changed PCA-DA to use logistic regression instead of linear regression.

--- R/pcr.R
+++ R/pcr.R
@@ -6,6 +6,7 @@ library(rlang)
 library(rsample)
 library(purrr)
 library(here)
+library(lmtest)
 source(here("R", "ropls_helpers.R"))
 
 #' Predict PC axis scores of new data from loadings
@@ -104,7 +105,7 @@ pca_RMSEP <- function(split, X_vars, Y_var) {
 #' @import rlang
 #' @import ropls
 #' @import holodeck
-#' 
+#' @importFrom lmtest lrtest
 #' @return
 #' @export
 #'
@@ -127,7 +128,7 @@ pcreg <- function(data, X_vars, Y_var, CV = 7){
       data %>%
       mutate(!!Y := as.numeric(!!Y) - 1)
   }
-  if(!is.integer(data[[quo_name(Y)]])){
+  if(!is.numeric(data[[quo_name(Y)]])){
     stop("grouping variable must be character, factor, or integer")
   }
   
@@ -150,6 +151,13 @@ pcreg <- function(data, X_vars, Y_var, CV = 7){
   # Do regression
   m <- glm(mod_form, family = "binomial", data = scores)
   
+  # get p-value and calc R2Y
+  lik.test <- lrtest(m)
+  
+  mod.stats <- broom::glance(m) %>%
+    mutate(R2Y = 1 - (deviance/null.deviance)) %>% 
+    add_column(p.value = lik.test$`Pr(>Chisq)`[2])
+  
   # Do CV
   df.cv <- rsample::vfold_cv(data, CV)
   RMSEP <- df.cv %>%
@@ -157,17 +165,42 @@ pcreg <- function(data, X_vars, Y_var, CV = 7){
     summarize(RMSEP = mean(RMSEP)) %>%
     as.numeric()
   
-  return(list(pca = pca, lm = m, RMSEP = RMSEP))
+  return(list(pca = pca, scores = scores, glm = m, mod.stats = mod.stats, RMSEP = RMSEP))
 }
-# set.seed(400)
-# library(holodeck)
-# test.df <- sim_cat(n_obs = 30, n_groups = 2) %>% 
-#   sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov") %>% 
-#   sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov2") %>% 
-#   # sim_covar(n_vars = 5, var = 1, cov = 0, name = "noise") %>% 
-#   group_by(group) %>% 
-#   sim_discr(n_vars = 5, var = 1, cov = 0.1, group_means = c(-1, 1), name = "discr") %>%
-#   ungroup() #%>% 
+
+
+
+set.seed(400)
+library(holodeck)
+data <- test.df <- sim_cat(n_obs = 30, n_groups = 2) %>%
+  sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov") %>%
+  sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov2") %>%
+  # sim_covar(n_vars = 5, var = 1, cov = 0, name = "noise") %>%
+  group_by(group) %>%
+  sim_discr(n_vars = 5, var = 1, cov = 0.1, group_means = c(-1, 1), name = "discr") %>%
+  ungroup() #%>%
+
+data <-
+  data %>%
+  mutate(group = as.factor(group)) %>%
+  mutate(group = as.numeric(group) - 1)
+
+pca <- opls(select(data, -group), plotL = FALSE, printL = FALSE)
+
+scores <- get_scores(pca) %>%
+  add_column(group = data[["group"]])
+
+npcs <- pca@summaryDF$pre
+pcs <- glue("p{1:npcs}")
+mod_form <- as.formula(glue::glue("group ~ {glue_collapse(pcs, sep = '+')}"))
+m <- glm(mod_form, family = "binomial", data = scores)
+
+glm <- broom::glance(m) %>%
+  mutate(R2Y = 1 - (deviance/null.deviance)) %>% 
+  add_column(p.value = lrtest(m)$`Pr(>Chisq)`[2])
+# 
+# test <-lrtest(m)
+# p.value <- test$`Pr(>Chisq)`[2]
 # # mutate(group = as.numeric(as.factor(group)))
 # m <- pcreg(X_vars = -group, Y_var = group, CV = 10, data = test.df)
 # m
