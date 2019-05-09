1e7ea45dcf86fe37786ee990b23309b7adefc01d
Aariq <scottericr@gmail.com>
2019-05-09 18:39:51 -0400

    now calculates R2Y by Tjur's definition

--- R/pcr.R
+++ R/pcr.R
@@ -150,13 +150,18 @@ pcreg <- function(data, X_vars, Y_var, CV = 7){
   
   # Do regression
   m <- glm(mod_form, family = "binomial", data = scores)
-  
+  m0 <- glm(group ~ 1, family = "binomial", data = scores)
   # get p-value and calc R2Y
-  lik.test <- lrtest(m)
-  
+  lik.test <- lrtest(m, m0)
+
   mod.stats <- broom::glance(m) %>%
-    mutate(R2Y = 1 - (deviance/null.deviance)) %>% 
-    add_column(p.value = lik.test$`Pr(>Chisq)`[2])
+    # mutate(R2Y = 1 - (deviance/null.deviance)) %>% 
+    add_column(R2Y = broom::augment(m, type.predict = "response") %>% 
+                 group_by(group) %>% 
+                 summarize(means = mean(.fitted)) %>%
+                 summarize(R2_tjur = diff(means)) %>%
+                 as.numeric(),
+               p.value = lik.test$`Pr(>Chisq)`[2])
   
   # Do CV
   df.cv <- rsample::vfold_cv(data, CV)
@@ -170,34 +175,34 @@ pcreg <- function(data, X_vars, Y_var, CV = 7){
 
 
 
-set.seed(400)
-library(holodeck)
-data <- test.df <- sim_cat(n_obs = 30, n_groups = 2) %>%
-  sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov") %>%
-  sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov2") %>%
-  # sim_covar(n_vars = 5, var = 1, cov = 0, name = "noise") %>%
-  group_by(group) %>%
-  sim_discr(n_vars = 5, var = 1, cov = 0.1, group_means = c(-1, 1), name = "discr") %>%
-  ungroup() #%>%
-
-data <-
-  data %>%
-  mutate(group = as.factor(group)) %>%
-  mutate(group = as.numeric(group) - 1)
-
-pca <- opls(select(data, -group), plotL = FALSE, printL = FALSE)
-
-scores <- get_scores(pca) %>%
-  add_column(group = data[["group"]])
-
-npcs <- pca@summaryDF$pre
-pcs <- glue("p{1:npcs}")
-mod_form <- as.formula(glue::glue("group ~ {glue_collapse(pcs, sep = '+')}"))
-m <- glm(mod_form, family = "binomial", data = scores)
-
-glm <- broom::glance(m) %>%
-  mutate(R2Y = 1 - (deviance/null.deviance)) %>% 
-  add_column(p.value = lrtest(m)$`Pr(>Chisq)`[2])
+# set.seed(400)
+# library(holodeck)
+# data <- test.df <- sim_cat(n_obs = 30, n_groups = 2) %>%
+#   sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov") %>%
+#   sim_covar(n_vars = 5, var = 1, cov = 0.5, name = "cov2") %>%
+#   # sim_covar(n_vars = 5, var = 1, cov = 0, name = "noise") %>%
+#   group_by(group) %>%
+#   sim_discr(n_vars = 5, var = 1, cov = 0.1, group_means = c(-1, 1), name = "discr") %>%
+#   ungroup() #%>%
+# 
+# data <-
+#   data %>%
+#   mutate(group = as.factor(group)) %>%
+#   mutate(group = as.numeric(group) - 1)
+# 
+# pca <- opls(select(data, -group), plotL = FALSE, printL = FALSE)
+# 
+# scores <- get_scores(pca) %>%
+#   add_column(group = data[["group"]])
+# 
+# npcs <- pca@summaryDF$pre
+# pcs <- glue("p{1:npcs}")
+# mod_form <- as.formula(glue::glue("group ~ {glue_collapse(pcs, sep = '+')}"))
+# m <- glm(mod_form, family = "binomial", data = scores)
+# 
+# glm <- broom::glance(m) %>%
+#   mutate(R2Y = 1 - (deviance/null.deviance)) %>% 
+#   add_column(p.value = lrtest(m)$`Pr(>Chisq)`[2])
 # 
 # test <-lrtest(m)
 # p.value <- test$`Pr(>Chisq)`[2]
